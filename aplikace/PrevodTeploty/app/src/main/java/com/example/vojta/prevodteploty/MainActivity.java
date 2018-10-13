1fca1dfd1c0e52d2ed0803f04896e9c2ca18d3e2
Vojtech Bocek <vbocek@gmail.com>
2018-10-13 18:36:34 +0200

    Change IDs in PrevodTeploty, use setOnClickListener

--- aplikace/PrevodTeploty/app/src/main/java/com/example/vojta/prevodteploty/MainActivity.java
+++ aplikace/PrevodTeploty/app/src/main/java/com/example/vojta/prevodteploty/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.vojta.prevodteploty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,19 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onPrevestClick(View button) {
-        TextView result = findViewById(R.id.resultText);
-        EditText input = findViewById(R.id.inputText);
-        try {
-            double cel = Double.parseDouble(input.getText().toString());
-            result.setText(celToFar(cel) + " °F");
-        } catch(NumberFormatException e) {
-            result.setText("Neplatný vstup!");
-        } catch(ArithmeticException e) {
-            result.setText(e.toString());
-        }
+        Button prevest = findViewById(R.id.prevestBtn);
+
+        prevest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView result = findViewById(R.id.vysledek);
+                EditText input = findViewById(R.id.vstup);
+                try {
+                    double cel = Double.parseDouble(input.getText().toString());
+                    result.setText(celToFar(cel) + " °F");
+                } catch(NumberFormatException e) {
+                    result.setText("Neplatný vstup!");
+                } catch(ArithmeticException e) {
+                    result.setText(e.toString());
+                }
+            }
+        });
     }
 
     double celToFar(double cel) throws ArithmeticException {
