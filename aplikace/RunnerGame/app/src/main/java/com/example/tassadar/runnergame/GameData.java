4389f8737410e0d29d78f155ff9e0d6378b9a25f
Vojtech Bocek <vbocek@gmail.com>
2019-02-17 11:45:21 +0100

    Implement Runner

---
+++ aplikace/RunnerGame/app/src/main/java/com/example/tassadar/runnergame/GameData.java
@@ -0,0 +1,116 @@
+package com.example.tassadar.runnergame;
+
+public class GameData {
+    public static final float PLAYER_POSITION = 0.33f;
+    public static final float PLAYER_WIDTH = 1.f / 20.f;
+    public static final float PLAYER_JUMP_MULT = 3.f;
+
+    private static final float COLLISION_MIN = PLAYER_POSITION - PLAYER_WIDTH*0.9f;
+    private static final float COLLISION_MAX = PLAYER_POSITION + PLAYER_WIDTH*0.9f;
+
+    private static final float TIME_JUMP = 300.f;
+    private static final float TIME_ONE_LENGTH = 1000.f;
+    private static final float TIME_MIN_BLOCK_DELAY = TIME_JUMP * 2;
+    private static final float TIME_MAX_BLOCK_DELAY = 2000.f;
+
+    private static final float SPEED_PLAYER = 1.f / TIME_ONE_LENGTH;
+    private static final float SPEED_JUMP = 1.f / (TIME_JUMP / 2.f);
+
+    public float groundBarsOffset;
+    public float jumpOffset;
+    public float[] blocks = new float[(int)(TIME_ONE_LENGTH / TIME_MIN_BLOCK_DELAY) + 1];
+    public double durationMs;
+    public boolean collision;
+
+    private float mJumpProgress;
+    private int mJumpState;
+    private float mBlockTimer;
+    private int mBlockIdx;
+
+    public GameData() {
+        reset();
+    }
+
+    public void reset() {
+        synchronized (this) {
+            durationMs = 0;
+            groundBarsOffset = 0.f;
+            jumpOffset = 0.f;
+            collision = false;
+
+            for (int i = 0; i < blocks.length; ++i) {
+                blocks[i] = 0;
+            }
+
+            mBlockTimer = 5000;
+            mJumpProgress = 0.f;
+            mJumpState = 0;
+            mBlockIdx = 0;
+        }
+    }
+
+    private void handleJump(float diffMs) {
+        switch(mJumpState) {
+            case 1:
+                mJumpProgress += SPEED_JUMP * diffMs;
+                jumpOffset = (float) ((Math.cos((mJumpProgress + 1) * Math.PI) / 2.f) + 0.5);
+                break;
+            case 2:
+                mJumpProgress += SPEED_JUMP * diffMs;
+                jumpOffset = 1.f - mJumpProgress * mJumpProgress;
+                break;
+        }
+
+        if(mJumpProgress >= 1.0f) {
+            mJumpProgress = 0.f;
+            ++mJumpState;
+            if(mJumpState == 3) {
+                mJumpState = 0;
+                jumpOffset = 0;
+            }
+        }
+    }
+
+    public boolean update(float diffMs) {
+        boolean crashed = false;
+        synchronized (this) {
+            durationMs += diffMs;
+            groundBarsOffset = (groundBarsOffset + SPEED_PLAYER * diffMs * 7.5f) % 1.0f;
+
+            handleJump(diffMs);
+
+            for(int i = 0; i < blocks.length; ++i) {
+                if(blocks.length > 0.f) {
+                    blocks[i] -= SPEED_PLAYER * diffMs;
+                    if(blocks[i] > COLLISION_MIN && blocks[i] < COLLISION_MAX && jumpOffset < 1.f/PLAYER_JUMP_MULT) {
+                        collision = true;
+                        crashed = true;
+                    }
+                }
+            }
+
+            if(mBlockTimer <= diffMs) {
+                blocks[mBlockIdx] = 1.f;
+                mBlockIdx = (mBlockIdx + 1) % blocks.length;
+                mBlockTimer = TIME_MIN_BLOCK_DELAY + ((float)Math.random())*TIME_MAX_BLOCK_DELAY;
+            } else {
+                mBlockTimer -= diffMs;
+            }
+        }
+        return crashed;
+    }
+
+    public void jump() {
+        synchronized (this) {
+            if(collision) {
+                reset();
+                return;
+            }
+
+            if(mJumpState == 0) {
+                mJumpState = 1;
+                mJumpProgress = 0.f;
+            }
+        }
+    }
+}
---
+++ aplikace/RunnerGame/app/src/main/java/com/example/tassadar/runnergame/GameThread.java
@@ -0,0 +1,56 @@
+package com.example.tassadar.runnergame;
+
+import android.view.View;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class GameThread extends Thread {
+    private static final int UPDATE_RATE = 60;
+    private static final long OPTIMAL_PERIOD = 1_000_000_000 / UPDATE_RATE;
+
+    private AtomicBoolean mPaused = new AtomicBoolean(true);
+    private GameData mData;
+    private View mGameView;
+
+    public GameThread(GameData data, View gameView) {
+        super();
+        setDaemon(true);
+
+        mData = data;
+        mGameView = gameView;
+    }
+
+    public void setPaused(boolean paused) {
+        mPaused.set(paused);
+    }
+
+    public boolean isPaused() {
+        return mPaused.get();
+    }
+
+    public void run() {
+        long now, sleepMs;
+        float diffMs;
+        long previous = System.nanoTime();
+        while(!this.isInterrupted()) {
+            now = System.nanoTime();
+            diffMs = (float)(now - previous) / 1_000_000;
+            previous = now;
+
+            if(!mPaused.get()) {
+                if(mData.update(diffMs)) {
+                    setPaused(true);
+                }
+                mGameView.postInvalidate();
+            }
+
+            sleepMs = Math.max(0, (previous - System.nanoTime() + OPTIMAL_PERIOD)) / 1_000_000;
+            try {
+                Thread.sleep(sleepMs);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
